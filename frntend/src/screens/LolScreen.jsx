d3b023d7f8f6ad88146379a17a6e94ce5eeb546b
suntwsmfg <63569220+suntwsmfg@users.noreply.github.com>
2020-09-18 14:47:40 +0530

    .

--- frntend/src/screens/LolScreen.jsx
+++ frntend/src/screens/LolScreen.jsx
@@ -1,6 +1,12 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 
 const LolScreen = () => {
+     const closeMenu = () => {
+          document.querySelector('.sidebar').classList.remove('open');
+        };
+     useEffect(() => {
+          closeMenu()
+     },[])
      return (
           <div>
                LolScreen
--- frntend/src/screens/SchedulingScreen.jsx
+++ frntend/src/screens/SchedulingScreen.jsx
@@ -1,6 +1,12 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 
 const SchedulingScreen = () => {
+     const closeMenu = () => {
+          document.querySelector('.sidebar').classList.remove('open');
+     };
+     useEffect(() => {
+          closeMenu()
+     }, [])
      return (
           <div>
                Scheduling Screeen
