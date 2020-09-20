74435f5cac28aa53e3c23fc055948d3328f9b318
suntwsmfg <63569220+suntwsmfg@users.noreply.github.com>
2020-09-20 14:59:52 +0530

    .

--- frntend/src/redux/scale/scaleActions.js
+++ frntend/src/redux/scale/scaleActions.js
@@ -5,7 +5,7 @@ import { SCALE_READING_FAIL, SCALE_READING_REQUEST, SCALE_READING_SUCCESS } from
 const scaleReading = () => async (dispatch) => {
      try {
           dispatch({ type: SCALE_READING_REQUEST });
-          const {data} = await scalConnection.get(`/scale`)
+          const {data} = await scalConnection.get(`/sc`)
           dispatch({type:SCALE_READING_SUCCESS,payload:data})
      } catch (error) {
           dispatch({ type: SCALE_READING_FAIL, payload: error.message });
