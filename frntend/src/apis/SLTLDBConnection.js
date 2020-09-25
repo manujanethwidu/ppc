6dfac750ab8a68f674972c8aa49891c37fc8b27d
suntwsmfg <63569220+suntwsmfg@users.noreply.github.com>
2020-09-25 10:18:27 +0530

    .

--- frntend/src/apis/SLTLDBConnection.js
+++ frntend/src/apis/SLTLDBConnection.js
@@ -1,5 +1,5 @@
 import axios from 'axios'
 
 export default axios.create({
-    baseURL:"http://192.168.1.3:3001" //<== no backslashe here
+    baseURL:"http://192.168.1.6:3001" //<== no backslashe here
 })
--- frntend/src/components/fi/FtrComp.js
+++ frntend/src/components/fi/FtrComp.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import styles from '../../css/FtrComp.module.css'
+import '../../css/FtrComp.css'
 const FtrComp = ({ tireDetails }) => {
      const { tiresizebasic,
           lugtype,
@@ -12,60 +12,53 @@ const FtrComp = ({ tireDetails }) => {
 
      } = tireDetails
      return (
-          <div className='fi-container'>
-               <div className='tire-detail'>
-                    <ul tire-detail-ul>
-                         <li>{tiresizebasic} {lugtype} {config}</li>
-                         <li>{rimsize}</li>
-                         <li>{tiretypecap}</li>
-                         <li>{brand} / {swmsg}</li>
-                         <li>{moldno}</li>
-                    </ul>
-               </div>
-               <div className="ftr-inputs">
-                    <ul>
-                         <li>
-                              Lug Damage: <select>
-                                   <option value={1}>1 Lugs</option>
-                                   <option value={2}>2 Lugs</option>
-                                   <option value={3}>3 Lugs</option>
-                                   <option value={4}>4 Lugs</option>
-                                   <option value={5}>5 Lugs</option>
-                                   <option value={6}>6 Lugs</option>
-                                   <option value={7}>7 Lugs</option>
-                                   <option value={8}>8 Lugs</option>
-                                   <option value={9}>9 Lugs</option>
-                                   <option value={10}>10 Lugs</option>
-                                   <option value={100}>10+ Lugs</option>
-                              </select>
-                         </li>
-                         <li>
-                              Bead Out: <select>
-                                   <option value={1}>1   Wires</option>
-                                   <option value={2}>2   Wires</option>
-                                   <option value={3}>3   Wires</option>
-                                   <option value={4}>4   Wires</option>
-                                   <option value={5}>5   Wires</option>
-                                   <option value={6}>6   Wires</option>
-                                   <option value={7}>7   Wires</option>
-                                   <option value={8}>8   Wires</option>
-                                   <option value={9}>9   Wires</option>
-                                   <option value={10}>10   Wires</option>
-                                   <option value={100}>10+   Wires</option>
-                              </select>
-                         </li>
-                         <li>
-                              Flow Mark: <select>
-                                   <option value={1}>Small</option>
-                                   <option value={2}>Medium</option>
-                                   <option value={3}>Large</option>
-                                   <option value={4}>Massive</option>
-                              </select>
-                         </li>
-                    </ul>
-               </div>
+          <div className=''>
+               <div className='ftr-container'>
+                    <div class="row">
+                         <div className='tire-detail'>
+                              {tiresizebasic} {lugtype} {config} {rimsize} {tiretypecap}
+                              <br />
+                              {brand} / {swmsg}
+                              <br/>
+                              MoldNo:-{moldno}
+                         
+                         </div>
+                      
+                         <div></div>
+                         </div>
+               <div class="row">
+                    <div class="col-5 menu">
+                         <ul>
+                              <li>Thick Flash</li>
+                              <li>The City</li>
+                              <li>The Island</li>
+                              <li>The Food</li>
+                         </ul>
+                    </div>
+                    <div class="col-5 select">
+                         <ul>
+                              <li>
+                                   <select>
+                                        <option >5mm</option>
+                                        <option >10mm</option>
+                                        <option >15mm</option>
+                                        </select>
+                                        
+                              </li>
+                              <li>The City</li>
+                              <li>The Island</li>
+                              <li>The Food</li>
+                         </ul>
+                    </div>
 
+               </div>
+               <div class="row">
+
+               </div>
+         </div>
           </div>
+
+
      )
 }
 
